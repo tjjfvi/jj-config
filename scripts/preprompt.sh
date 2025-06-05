d9fbd0a6e6bbb1be5153ae503e07ba6a8a1a9b05
vjjft <vjjft@vjjft.com>
2025-06-05 12:47:12 -0700

    fix preprompt re pager

--- scripts/preprompt.sh
+++ scripts/preprompt.sh
@@ -2,7 +2,7 @@
 if jj root &>/dev/null; then
   OP_ID=`jj cur-op`
   if [[ $OP_ID != $_PROMPT_LAST_OP_ID ]] && [[ `jj pure op log -n1 --no-graph -T snapshot` != "true" ]]; then
-    PS1="\n`jj pure color preprompt`\n$PS1"
+    PS1="\n`jj --no-pager pure color preprompt`\n$PS1"
   fi
-  export _PROMPT_LAST_OP_ID=$OP_ID
+  export _PROMPT_LAST_OP_ID="$OP_ID"
 fi
